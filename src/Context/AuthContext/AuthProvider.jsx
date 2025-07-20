da243427604c4af58fb56cb61018ea68fef3ec60
user <foysalhaque.mist.ipe@gmail.com>
2025-07-20 09:13:09 +0600

    createUser,signIn added

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { AuthContext } from './AuthContet';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../Firebase/Firebase.init';
 
 const AuthProvider = ({children}) => {
 
+    
+    const createUser = (email,password) =>{
+       return createUserWithEmailAndPassword(auth,email,password)
+    };
+
+    const signIn = (email,password) =>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+
+
     const authInfo = {
+        createUser,
+        signIn
 
     }
 
